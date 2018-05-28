2a3d3f050b4c218f86c0db42118faa1d32347c36
OblivionSan <oblivionsan@outlook.com>
2018-05-28 22:27:44 +0100

    Updated everything.

--- index.php
+++ index.php
@@ -1,119 +1,139 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Weeb | Home</title>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/ghWidget.js"></script>
+    <meta name="description" content="A home for weeb based projects">
+    <meta name="keywords" content="weeb, weebs, weebs.cf, oblivionsan, oblivion">
+    <meta name="theme-color" content="EDABD1">
+    <meta property="og:image" content="https://weebs.cf/assets/img/girl.jpg"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://weebs.cf/"/>
+    <meta property="og:site_name" content="Weebs.CF"/>
+    <meta property="og:title" content="Home | Weebs.CF"/>
+    <meta property="og:description" content="A home for weeb based projects"/>
+    <link rel="icon" href="assets/img/girl.jpg" type="image/x-icon"/>
+    <link rel="shortcut icon" href="assets/img/girl.jpg"type="image/x-icon"/>
+    <title>Home | Weeb</title>
+    <!-- CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/fontawesome.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
-    <link rel="shortcut icon" href="assets/img/girl.jpg"type="image/x-icon" />
+    <?php require_once 'assets/api/Mobile_Detect.php'; ?>
+    <?php require_once 'assets/api/detect.php'; ?>
   </head>
-  <body>
-    <div class="container"></div>
-    <div class="container" style="padding-top:25px;">
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="./"><b>Weebs.CF</b></a>
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li><a href="https://github.com/OblivionSan" target="_blank"><i class="fa fa-github" aria-hidden="true"></i> GitHub</a></li>
-              <li><a href="https://twitter.com/OblivionSan" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a></li>
-              <li><a href="https://discord.oblivionsan.tk/" target="_blank"><i class="fa fa-comments" aria-hidden="true"></i> Discord</a></li>
-              <li><a href="random/"><i class="fa fa-code" aria-hidden="true"></i> Example</a></li>
-              <li><a target="_blank" href="kitsu/"><i class="fa fa-code" aria-hidden="true"></i> Kitsu-Widget</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+  <body class="bg-light">
     <div class="container">
-      <h2>Home - <small>A home for <b>weeb</b> based projects</small></h2>
-      <hr>
-      <div class="row">
-      </div>
-      <div class="panel panel-default">
-        <div class="panel-body">
-          <h3>Random Image List.</h3>
-          <br>
-          <table>
-            <tr>
-              <th>Directory Names</th>
-              <th>Directory Links</th>
-            </tr>
-            <tr>
-              <td>Hugs</td>
-              <td><a href="random/hug">Click Here</a></td>
-            </tr>
-            <tr>
-              <td>Punches</td>
-              <td><a href="random/punch">Click Here</a></td>
-            </tr>
-            <tr>
-              <td>Poke</td>
-              <td><a href="random/poke">Click Here</a></td>
-            </tr>
-            <tr>
-              <td>Pat</td>
-              <td><a href="random/pat">Click Here</a></td>
-            </tr>
-            <tr>
-              <td>Kiss</td>
-              <td><a href="random/kiss">Click Here</a></td>
-            </tr>
-            <tr>
-              <td>Slap</td>
-              <td><a href="random/slap">Click Here</a></td>
-            </tr>
-            <tr>
-              <td>Laugh</td>
-              <td><a href="random/laugh">Click Here</a></td>
-            </tr>
-            <tr>
-              <td>Coin Flip</td>
-              <td><a href="random/coin">Click Here</a></td>
-            </tr>
-          </table>
-          <br>
+ <!-- Fixed navbar -->
+ <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Weebs.CF</a>
         </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="../"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+            <li><a href="random/"><i class="fa fa-code" aria-hidden="true"></i> Examples</a></li>
+            <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-share-square" aria-hidden="true"></i> Socials <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a target="_blank" href="https://discord.oblivionsan.tk"><i class="fab fa-discord" aria-hidden="true"></i> Discord</a></li>
+                  <li><a target="_blank" href="https://twitter.com/OblivionSan"><i class="fab fa-twitter" aria-hidden="true"></i> Twitter</a></li>
+                  <li><a target="_blank" href="https://github.com/OblivionSan"><i class="fab fa-github" aria-hidden="true"></i> GitHub</a></li>
+                </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
-      <div class="panel panel-default">
-        <div class="panel-body">
-        <h3>GitHub List.</h3>
-          <!--Panel-->
-          <div style="padding: 10px" class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <div class="github-widget" data-username="WeebIndex"></div>
-              </div>
-            </div>
-          </div>
-          <div style="padding: 10px" class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <div class="github-widget" data-username="OblivionSan"></div>
-              </div>
-            </div>
-          </div>
-          <div style="padding: 10px" class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <div class="github-widget" data-username="MizoreChan"></div>
-              </div>
-            </div>
-          </div>
-          <!--/.Panel-->
+    </nav>
+      <h2 style="padding-top: 50px">Home - <small>A home for <b>weeb</b> based projects</small></h2>
+      <br>
+      <button class="accordion"><i class="fas fa-chevron-right" aria-hidden="true"></i> 🖼️ Random Image List</button>
+      <div class="panel">
+        <br>
+        <h3>Anime</h3>
+        <table>
+          <tr>
+            <th>Directory Names</th>
+            <th>Directory Links</th>
+          </tr>
+          <tr>
+            <td>Hugs</td>
+            <td><a href="random/hug">Click Here</a></td>
+          </tr>
+          <tr>
+            <td>Punches</td>
+            <td><a href="random/punch">Click Here</a></td>
+          </tr>
+          <tr>
+            <td>Poke</td>
+            <td><a href="random/poke">Click Here</a></td>
+          </tr>
+          <tr>
+            <td>Pat</td>
+            <td><a href="random/pat">Click Here</a></td>
+          </tr>
+          <tr>
+            <td>Kiss</td>
+            <td><a href="random/kiss">Click Here</a></td>
+          </tr>
+          <tr>
+            <td>Slap</td>
+            <td><a href="random/slap">Click Here</a></td>
+          </tr>
+          <tr>
+            <td>Laugh</td>
+            <td><a href="random/laugh">Click Here</a></td>
+          </tr>
+        </table>
+        <h3>Other</h3>
+        <table>
+          <tr>
+            <th>Directory Names</th>
+            <th>Directory Links</th>
+          </tr>
+          <tr>
+            <td>Coin Flip</td>
+            <td><a href="random/coin">Click Here</a></td>
+          </tr>
+        </table>
+        <br>
+      </div>
+      <button class="accordion"><i class="fas fa-chevron-right" aria-hidden="true"></i> 🦊 Kitsu-Widget <a target="_blank" href="kitsu/">[LINK]</a></button>
+      <div class="panel">
+        <iframe src="kitsu" frameborder="0" height="500px" width="100%"></iframe>
+        <br>
+      </div>
+      <button class="accordion"><i class="fas fa-chevron-right" aria-hidden="true"></i> 💻 GitHub List</button>
+      <div class="panel">
+        <br>
+        <div style="padding-bottom: 10px" class="col-sm-4">
+          <div class="github-widget" data-username="WeebIndex"></div>
         </div>
+        <div style="padding-bottom: 10px" class="col-sm-4">
+          <div class="github-widget" data-username="OblivionSan"></div>
+        </div>
+        <div style="padding-bottom: 10px" class="col-sm-4">
+            <div class="github-widget" data-username="MizoreChan"></div>
+          </div>
+        <br>
       </div>
-    </div>
+    <!-- /container -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/ghWidget.js"></script>
     <div class="container text-center" style="padding-bottom:50px;">
       <hr>
+      <small class="text-muted">Your IP: <?php echo Detect::ip();?></small>
+      <br>
       <small class="text-muted">Website developed by <a href="https://oblivionsan.tk/" target="_blank"><b>Oblivion さん</b></a></small>
       <br>
     </div>
--- random/index.php
+++ random/index.php
@@ -1,69 +1,91 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Weeb | Example</title>
-    <script src="../assets/js/bootstrap.min.js"></script>
-    <script src="../assets/js/jquery.min.js"></script>
+    <meta name="description" content="A home for weeb based projects">
+    <meta name="keywords" content="weeb, weebs, weebs.cf, oblivionsan, oblivion">
+    <meta name="theme-color" content="EDABD1">
+    <meta property="og:image" content="https://weebs.cf/assets/img/girl.jpg"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://weebs.cf/random/"/>
+    <meta property="og:site_name" content="Weebs.CF"/>
+    <meta property="og:title" content="Examples | Weebs.CF"/>
+    <meta property="og:description" content="A home for weeb based projects"/>
+    <link rel="icon" href="../assets/img/girl.jpg" type="image/x-icon"/>
+    <link rel="shortcut icon" href="../assets/img/girl.jpg"type="image/x-icon"/>
+    <title>Examples | Weeb</title>
+    <!-- CSS -->
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../assets/css/fontawesome.min.css" rel="stylesheet">
     <link href="../assets/css/style.css" rel="stylesheet">
-    <link rel="shortcut icon" href="../assets/img/girl.jpg"type="image/x-icon" />
     <!-- Assets for HighLight.js -->
     <link rel="stylesheet" href="../assets/css/github.css">
     <script src="../assets/js/highlight.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
+    <!-- PHP -->
+    <?php require_once '../assets/api/Mobile_Detect.php'; ?>
+    <?php require_once '../assets/api/detect.php'; ?>
   </head>
-  <body>
-    <div class="container"></div>
-    <div class="container" style="padding-top:25px;">
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="../"><b>Weebs.CF</b></a>
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li><a href="https://github.com/OblivionSan" target="_blank"><i class="fa fa-github" aria-hidden="true"></i> GitHub</a></li>
-              <li><a href="https://twitter.com/OblivionSan" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a></li>
-              <li><a href="https://discord.oblivionsan.tk/" target="_blank"><i class="fa fa-comments" aria-hidden="true"></i> Discord</a></li>
-              <li><a href="./"><i class="fa fa-code" aria-hidden="true"></i> Example</a></li>
-              <li><a target="_blank" href="../kitsu/"><i class="fa fa-code" aria-hidden="true"></i> Kitsu-Widget</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+  <body class="bg-light">
     <div class="container">
-      <h2><a href="../">Home</a> / Example - <small>A home for <b>weeb</b> based projects</small></h2>
+       <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Weebs.CF</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="../"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+            <li class="active"><a href="#"><i class="fa fa-code" aria-hidden="true"></i> Examples</a></li>
+            <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-share-square" aria-hidden="true"></i> Socials <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a target="_blank" href="https://discord.oblivionsan.tk"><i class="fab fa-discord" aria-hidden="true"></i> Discord</a></li>
+                  <li><a target="_blank" href="https://twitter.com/OblivionSan"><i class="fab fa-twitter" aria-hidden="true"></i> Twitter</a></li>
+                  <li><a target="_blank" href="https://github.com/OblivionSan"><i class="fab fa-github" aria-hidden="true"></i> GitHub</a></li>
+                </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+      <h2 style="padding-top: 50px">Examples - <small>A home for <b>weeb</b> based projects</small></h2>
+      <br>
+      <div class="container back">
+      <h3>How to use <a href="../">Weebs.CF</a> - Random Image Feature</h3>
       <hr>
-      <div class="panel panel-default">
-        <div class="panel-body">
-          <h2>
-          How to use <a href="../">Weebs.CF</a> - Random Image Feature</h4>
-          <P>These examples will give you an idea on how to use Weebs.CF's <b>Random Image</b> feature.<br>These examples will be using the <a href="https://weebs.cf/random/hug" target="_blank">Hug</a> directory.<br>If you have any other usage examples for <a href="../">Weebs.CF</a>'s <b>Random Image</b> feature then feel free to let me know via my <a href="https://discord.oblivionsan.tk">Discord</a>.</P>
-          <h3>JavaScript</h3>
-          <br>
-          <pre><code class="javascript">const request = require("snekfetch");
+      <p>These examples will give you an idea on how to use <a href="../">Weebs.CF</a>'s <b>Random Image</b> feature.<br>These examples will be using the <a href="../random/hug">Hug</a> directory.<br>If you have any other usage examples for <a href="../">Weebs.CF</a>'s <b>Random Image</b> feature then feel free to let me know via my <a href="https://discord.oblivionsan.tk">Discord</a>.</p>
+      <hr>
+      <h3>JavaScript</h3>
+          <hr>
+          <pre><code class="javascript">//Known issues with snekfetch@4.0.0 ~ install snekfetch@3.6.1
+const request = require("snekfetch");
 request.get("https://weebs.cf/random/hug").then(body => {
     console.log(body.text)
 });
 </code></pre>
           <h3>PHP</h3>
-          <br>
+          <hr>
           <pre><code class="php">$text = file_get_contents("https://weebs.cf/random/hug");
 echo $text;
 </code></pre>
           <h3>Python</h3>
-          <br>
+          <hr>
           <pre><code class="python">import requests
 response = requests.get("https://weebs.cf/random/hug")
 print(response.text)
 </code></pre>
           <h3>Go</h3>
-          <br>
+          <hr>
           <pre><code class="go">package main
 import (
 	"fmt"
@@ -83,12 +105,16 @@ func main() {
 	}
 	fmt.Printf("%s\n", html)
 }
-</code></pre>
-        </div>
-      </div>
-    </div>
+</code></pre></div>
+    <!-- /container -->
+    <script src="../assets/js/main.js"></script>
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/ghWidget.js"></script>
     <div class="container text-center" style="padding-bottom:50px;">
       <hr>
+      <small class="text-muted">Your IP: <?php echo Detect::ip();?></small>
+      <br>
       <small class="text-muted">Website developed by <a href="https://oblivionsan.tk/" target="_blank"><b>Oblivion さん</b></a></small>
       <br>
     </div>
